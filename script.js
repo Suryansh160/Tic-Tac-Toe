bd0c73f744e8b4b2cb9634abdb1483c02fb5f3c1
Suryansh160 <suryanshgupta160@gmail.com>
2024-11-28 16:01:15 +0530

    updates

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let boxes=document.querySelectorAll(".box");
-let resetBtn=document.querySelectorAll(".Reset");
+let resetBtn=document.querySelector(".Reset");
 let turno=true;
 let newGameBtn=document.querySelector(".new-btn");
 let msgCont=document.querySelector(".msg-container");
@@ -24,14 +24,14 @@ boxes.forEach((box)=>{
         console.log("s");
         if(turno===true){
 
-            box.innerHTML = '<img src="./img/img1.png" alt="x"  width="120">';
+            box.innerHTML = '<img src="./img/img1.png" alt="x"  width="120">X</img>';
 
            
             turno=false;
         }
         else if(turno===false)
         {
-            box.innerHTML = '<img src="./img/img2.png" alt="x"  width="120">';
+            box.innerHTML = '<img src="./img/img2.png" alt="x"  width="120">O</img>';
             turno=true;
         }
         box.disabled=true;
@@ -53,16 +53,19 @@ const enableBoxes=()=>{
 }
 
 const showWinner=(winner)=>{
-msg.innerText=`Congratulations, Winner is ${winner}`;
+    if(winner==="X")
+msg.innerText=`Congratulations, Winner is ${"Player 1"}`;
+    else
+    msg.innerText=`Congratulations, Winner is ${"Player 2"}`;
 msg.classList.remove("hide");
 disableBoxes();
 }
 
 const checkWinner=()=>{
     for(let pattern of winPatterns){
-        let pos1Val=boxes[pattern[0]].innerHTML;
-        let pos2Val=boxes[pattern[1]].innerHTML;
-        let pos3Val=boxes[pattern[2]].innerHTML;
+        let pos1Val=boxes[pattern[0]].innerText;
+        let pos2Val=boxes[pattern[1]].innerText;
+        let pos3Val=boxes[pattern[2]].innerText;
 
         if(pos1Val!=""&&pos2Val!=""&&pos3Val!=""){
             if(pos1Val===pos2Val&&pos2Val===pos3Val){
